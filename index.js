1d473d081c63d381f97f6f60affe48347c700bc6
juhaina1996 <juhaina.mt@rariti.io>
2022-08-12 12:06:44 +0530

    map  function

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 let numbers = [-23,-20,-17, -12, -5, 0, 1, 5, 12, 19, 20];
 
 let positive_array = numbers.filter(func1);
-function func1(value) {
-    return value >= 10; }
+function func1(value) 
+{
+    return value >= 10; 
+}
 
 console.log(positive_array);
+const array1 = [1, 4, 9, 16];
+const map1 = array1.map(a => a % 2);
+
+console.log(map1);
