84c12482ebb9a7c868e40937ce846baf38598cce
juhaina1996 <juhaina.mt@rariti.io>
2022-08-13 15:33:17 +0530

    DW-80: promise feauture in Js

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 setTimeout(()=>
     {
-        console.log("Hai Juhaina")
+        console.log("Hai Juhaina!!!")
      }, 1000);
      console.log("How are you")   //this will print "How are you" rather than printing "Hai Juhaina". So in order to solve that we have to use the promise feauture in JS
 //about promise in Javascript
